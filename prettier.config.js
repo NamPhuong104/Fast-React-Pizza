70274cd27a519e68485fb37c5553df76039dd2ee
Nam Phuong <nguyennamphuong104@gmail.com>
2024-07-31 23:01:05 +0700

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+import tailwindcssPlugin from "prettier-plugin-tailwindcss";
+module.exports = {
+  plugins: [tailwindcssPlugin],
+  singleQuote: true,
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+};
